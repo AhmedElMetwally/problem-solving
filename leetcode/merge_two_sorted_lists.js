5e391c0c269a6f236ee46e82135e7c012843c04f
Ahmed ElMetwally <ahmed.el.metwally.official@gmail.com>
2020-09-20 00:29:42 +0200

    add new javascript solution for leetcode "merge-two-sorted-lists"

--- leetcode/merge_two_sorted_lists.js
+++ leetcode/merge_two_sorted_lists.js
@@ -2,7 +2,6 @@
 // Link: https://leetcode.com/problems/merge-two-sorted-lists
 //
 
-
 /**
  *
  *
@@ -12,6 +11,7 @@
  *     this.next = (next===undefined ? null : next)
  * }
  */
+
 /**
  * Solution 1
  *
@@ -39,6 +39,28 @@ const mergeTwoLists = (l1, l2) => {
     current.next = l1;
   } else {
     current.next = l2;
+  }
+};
+
+/**
+ * Solution 2
+ *
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+const mergeTwoLists = (l1, l2) => {
+  const head = new ListNode();
+
+  if (!l1) return l2;
+  if (!l2) return l1;
+
+  if (l1.val > l2.val) {
+    head.next = l2;
+    head.next.next = mergeTwoLists(l1, l2.next);
+  } else {
+    head.next = l1;
+    head.next.next = mergeTwoLists(l1.next, l2);
   }
 
   return head.next;
